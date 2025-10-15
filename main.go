12529fe3c9a199116a0c4866f28ba26a15d6397f
andamorim2206 <deds5879@gmail.com>
2025-10-15 13:58:50 -0300

    feat: create scheme

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/andamorim2206/gopportunities/router"
+import (
+	"github.com/andamorim2206/gopportunities/config"
+	"github.com/andamorim2206/gopportunities/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+	// Initializer Confings
+	err := config.Init()
+	if err != nil {
+		logger.Errof("config initialization error: %v", err)
+		return
+	}
 
 	// Initializer router
 	router.Initialize()
